3e305c85724a1d257afcd832078b41fb79a67e3f
João Carlos Barbosa dos Santos <jbarsan@yandex.com>
2017-07-13 16:13:35 -0300

    Exercício 01 - Tópicos Especiais em Programação

--- mysite/pools/urls.py
+++ mysite/pools/urls.py
@@ -5,4 +5,6 @@ from . import views
 urlpatterns = [
     url(r'^$', views.index, name='index'),
     url(r'^question/(?P<question_id>\d+)$', views.exibir, name='exibir'),
+    url(r'^question/(?P<question_id>\d+)/results$', views.results, name='resultados'),
+    url(r'^question/(?P<question_id>\d+)/vote$', views.vote, name='voto'),
 ]
--- mysite/pools/views.py
+++ mysite/pools/views.py
@@ -1,14 +1,31 @@
 from django.shortcuts import render
-from .models import Question
+from .models import Question, Choice
 
 # Create your views here.
 
+
 def index(request):
-    question = Question.objects.all()
-    return render(request, 'index.html',
-                  {'questions': question})
+    questions = Question.objects.order_by('-pub_date')
+    contexto = {'questions': questions}
+
+    return render(request, 'index.html', contexto)
+
 
 def exibir(request, question_id):
     question = Question.objects.get(id=question_id)
-    return render(request, 'question.html',
-                  {'question': question})
+    choices = question.choice_set.all()
+    contexto = {'question': question, 'choices': choices}
+
+    return render(request, 'question.html', contexto)
+
+
+def results(request, question_id):
+    pass
+
+
+def vote(request, question_id):
+    pass
+
+
+def manage(request):
+    pass
